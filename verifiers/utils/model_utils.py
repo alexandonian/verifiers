44ae969e63f0eb50df25af3bae3473de4f0a9c64
Alex Andonian <alexandonian@gmail.com>
2025-04-03 03:00:40 -0400

    Formatting

--- verifiers/utils/model_utils.py
+++ verifiers/utils/model_utils.py
@@ -8,6 +8,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 def is_liger_available() -> bool:
     return find_spec("liger_kernel") is not None
 
+
 def get_model(model_name: str, model_kwargs: Union[Dict[str, Any], None] = None) -> Any:
     if model_kwargs is None:
         model_kwargs = dict(
@@ -15,13 +16,15 @@ def get_model(model_name: str, model_kwargs: Union[Dict[str, Any], None] = None)
             attn_implementation="flash_attention_2",
             use_cache=False,
         )
-    if is_liger_available():
-        print("Using Liger kernel")
-        from liger_kernel.transformers import AutoLigerKernelForCausalLM # type: ignore
-        return AutoLigerKernelForCausalLM.from_pretrained(model_name, **model_kwargs)
-    else:
+    if not is_liger_available():
         return AutoModelForCausalLM.from_pretrained(model_name, **model_kwargs)
-    
+
+    print("Using Liger kernel")
+    from liger_kernel.transformers import AutoLigerKernelForCausalLM  # type: ignore
+
+    return AutoLigerKernelForCausalLM.from_pretrained(model_name, **model_kwargs)
+
+
 def get_tokenizer(model_name: str) -> Any:
     # tokenizer = None
     # if "Instruct" in model_name:
@@ -35,12 +38,17 @@ def get_tokenizer(model_name: str) -> Any:
     #     tokenizer.pad_token = tokenizer.eos_token
     #     tokenizer.pad_token_id = tokenizer.eos_token_id
     if not hasattr(tokenizer, "chat_template"):
-        raise ValueError(f"Tokenizer for model {model_name} does not have chat_template attribute, \
+        raise ValueError(
+            f"Tokenizer for model {model_name} does not have chat_template attribute, \
                             and could not find a tokenizer with the same name as the model with suffix \
-                            '-Instruct'. Please provide a tokenizer with the chat_template attribute.")
+                            '-Instruct'. Please provide a tokenizer with the chat_template attribute."
+        )
     return tokenizer
-            
-def get_model_and_tokenizer(model_name: str, model_kwargs: Union[Dict[str, Any], None] = None) -> Tuple[Any, Any]:
+
+
+def get_model_and_tokenizer(
+    model_name: str, model_kwargs: Union[Dict[str, Any], None] = None
+) -> Tuple[Any, Any]:
     model = get_model(model_name, model_kwargs)
     tokenizer = get_tokenizer(model_name)
     return model, tokenizer
