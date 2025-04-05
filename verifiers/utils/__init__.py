98047df879f703ea1f66e02bdd180b5475295d0f
Alex Andonian <alexandonian@gmail.com>
2025-04-05 00:09:49 -0400

    Refactor imports and update __all__ to include write_json

--- verifiers/utils/__init__.py
+++ verifiers/utils/__init__.py
@@ -1,18 +1,26 @@
-from .data_utils import extract_boxed_answer, extract_hash_answer, preprocess_dataset, format_prompt, format_dataset
 from .config_utils import get_default_grpo_config
-from .model_utils import get_model, get_tokenizer, get_model_and_tokenizer
-from .logging_utils import setup_logging, print_prompt_completions_sample
+from .data_utils import (
+    extract_boxed_answer,
+    extract_hash_answer,
+    format_dataset,
+    format_prompt,
+    preprocess_dataset,
+    write_json,
+)
+from .logging_utils import print_prompt_completions_sample, setup_logging
+from .model_utils import get_model, get_model_and_tokenizer, get_tokenizer
 
 __all__ = [
     "extract_boxed_answer",
     "extract_hash_answer",
-    "preprocess_dataset",
-    "format_prompt",
     "format_dataset",
+    "format_prompt",
     "get_default_grpo_config",
     "get_model",
-    "get_tokenizer",
     "get_model_and_tokenizer",
-    "setup_logging",
+    "get_tokenizer",
+    "preprocess_dataset",
     "print_prompt_completions_sample",
+    "setup_logging",
+    "write_json",
 ]
